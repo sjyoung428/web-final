9dcb774bcac7f8ba478ab0035a037f95d341ba41
sjyoung428 <sjy8267@naver.com>
2023-06-07 16:08:25 +0900

    chat item css

--- client/src/components/chat/ChatItem.tsx
+++ client/src/components/chat/ChatItem.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Avatar from "../common/Avatar";
 import { DefaultSession } from "next-auth";
 import { useSession } from "next-auth/react";
+import { cls } from "@/utils/cls";
 
 interface ChatItemProps {
   children: React.ReactNode;
@@ -26,20 +27,25 @@ const ChatItem = ({ children, user }: ChatItemProps) => {
 
   return (
     <>
-      {me === false && (
-        <li className="flex items-center gap-2">
-          <Avatar image={user.image} size="sm" />
-          <span>{user.name}</span>
-          <span>{children}</span>
-        </li>
-      )}
-      {me === true && (
-        <li className="flex justify-end items-center gap-2">
-          <span>{user.name}</span>
-          <span>{children}</span>
-          <Avatar image={user?.image} size="sm" />
-        </li>
-      )}
+      <li
+        className={cls(
+          `w-full flex items-center gap-2 ${me ? "justify-end" : ""}`
+        )}
+      >
+        <div
+          className={cls(
+            `w-full flex flex-col ${
+              me ? "items-end" : "items-start order-last"
+            }`
+          )}
+        >
+          <span className="mb-2 font-thin text-sm">{user.name}</span>
+          <span className="bg-slate-200 rounded-md text-slate-900 p-4 max-w-[50%] whitespace-pre-wrap">
+            {children}
+          </span>
+        </div>
+        <Avatar image={user?.image} size="sm" />
+      </li>
     </>
   );
 };
