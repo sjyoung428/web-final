22304c25cfb1541eb4d59cbd2cab0739226221a7
sjyoung428 <sjy8267@naver.com>
2023-06-07 19:37:54 +0900

    chat form

--- client/src/components/chat/ChatForm.tsx
+++ client/src/components/chat/ChatForm.tsx
@@ -1,12 +1,38 @@
 "use client";
 
+import { useForm } from "react-hook-form";
 import Button from "../common/Button";
+import { useSession } from "next-auth/react";
 
 const ChatForm = () => {
+  const { data: session } = useSession();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
+    reset();
+  };
+
   return (
-    <form className="flex border w-[80%] relative">
-      <input className="w-full py-3 px-4" type="text" />
+    <form
+      className="flex border w-[80%] relative"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input
+        className="w-full py-3 px-4"
+        type="text"
+        {...register("chat", { required: true })}
+      />
       <Button className="absolute top-0 right-0">채팅 입력</Button>
+      {errors.chat && (
+        <span className="absolute bottom-[-25px] left-0">
+          채팅을 입력해주세요.
+        </span>
+      )}
     </form>
   );
 };
