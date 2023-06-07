89d63f88d8d56d5f05ef64d497caaee5ad313491
sjyoung428 <sjy8267@naver.com>
2023-06-07 20:29:49 +0900

    useUser hook

---
+++ client/src/hooks/useUser.ts
@@ -0,0 +1,13 @@
+import { useSession } from "next-auth/react";
+
+interface User {
+  name: string;
+  email: string;
+  image: string;
+}
+
+export const useUser = () => {
+  const { data: session } = useSession();
+  if (!session) throw new Error("로그인 하세요");
+  return session.user as User;
+};
